Yepoleb/dayzquery

commit bdab767a9c14e963513ce78b05b12b46220a8964

Fix timeout and encoding propagation
Closes #1

dayzquery.py
@@ -63,11 +63,11 @@ class DayzRules(metaclass=DataclsMeta):
 
 
 def dayz_rules(address, timeout=DEFAULT_TIMEOUT, encoding=DEFAULT_ENCODING):
-    rules_resp = a2s.rules(address, encoding=None)
+    rules_resp = a2s.rules(address, timeout, encoding)
     return dayz_rules_decode(rules_resp, encoding)
 
 async def dayz_arules(address, timeout=DEFAULT_TIMEOUT, encoding=DEFAULT_ENCODING):
-    rules_resp = await a2s.arules(address, encoding=None)
+    rules_resp = await a2s.arules(address, timeout, encoding)
     return dayz_rules_decode(rules_resp, encoding)
 
 

commit 7f7451119a0abe613dd932a7c247a28465dab3a9

Fix encoding crash caused by bdab767a9c14e963513ce78b05b12b46220a8964

dayzquery.py
@@ -63,11 +63,11 @@ class DayzRules(metaclass=DataclsMeta):
 
 
 def dayz_rules(address, timeout=DEFAULT_TIMEOUT, encoding=DEFAULT_ENCODING):
-    rules_resp = a2s.rules(address, timeout, encoding)
+    rules_resp = a2s.rules(address, timeout, encoding=None)
     return dayz_rules_decode(rules_resp, encoding)
 
 async def dayz_arules(address, timeout=DEFAULT_TIMEOUT, encoding=DEFAULT_ENCODING):
-    rules_resp = await a2s.arules(address, timeout, encoding)
+    rules_resp = await a2s.arules(address, timeout, encoding=None)
     return dayz_rules_decode(rules_resp, encoding)
 
 